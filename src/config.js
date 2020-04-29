3447dfec0d435ea64f3b624d6c740807ba1403e9
mrmadhat <daniel@relativemarketing.co.uk>
2020-04-29 15:16:02 +0100

    Setting up the scratch app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "mmhnotes",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ubtoskhoxl.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_udJetNsWD",
+    APP_CLIENT_ID: "2ka7007l9laj2b3p0mdgfebfvo",
+    IDENTITY_POOL_ID: "us-east-1:3183b2b8-e007-4828-95da-e42c4fd992e6",
+  },
+}
