12fddce3aa209085691f7b2e93cd1dd446572cae
mrmadhat <daniel@relativemarketing.co.uk>
2020-04-30 15:59:44 +0100

    Building react app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react"
+import { Auth } from "aws-amplify"
+
+import { Link, useHistory } from "react-router-dom"
 import { LinkContainer } from "react-router-bootstrap"
 import { Nav, Navbar, NavItem } from "react-bootstrap"
+import { AppContext } from "./libs/contextLib"
 import "./App.css"
 
 import Routes from "./Routes"
+import { onError } from "./libs/errorLib"
 
 function App() {
+  const history = useHistory()
+  const [isAuthenticated, userHasAuthenticated] = useState(false)
+  const [isAuthenticating, setIsAuthenticating] = useState(true)
+
+  useEffect(() => {
+    onload()
+  }, [])
+
+  async function onload() {
+    try {
+      await Auth.currentSession()
+      userHasAuthenticated(true)
+    } catch (e) {
+      if (e !== "No current user") {
+        onError(e)
+      }
+    }
+
+    setIsAuthenticating(false)
+  }
+
+  async function handleLogout() {
+    await Auth.signOut()
+
+    userHasAuthenticated(false)
+
+    history.push("/login")
+  }
+
   return (
+  !isAuthenticating && (
     <div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
@@ -18,18 +52,34 @@ function App() {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
+            {isAuthenticated ? (
+              <>
+                <LinkContainer to="/settings">
+                  <NavItem>Settings</NavItem>
+                </LinkContainer>
+                <NavItem onClick={handleLogout}>Logout</NavItem>
+              </>
+            ) : (
+              <>
+                <LinkContainer to="/signup">
+                  <NavItem>Signup</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <NavItem>Login</NavItem>
+                </LinkContainer>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider
+        value={{ isAuthenticated, userHasAuthenticated }}
+      >
+        <Routes />
+      </AppContext.Provider>
     </div>
   )
+);
 }
 
 export default App
--- src/config.js
+++ src/config.js
@@ -13,4 +13,6 @@ export default {
     APP_CLIENT_ID: "2ka7007l9laj2b3p0mdgfebfvo",
     IDENTITY_POOL_ID: "us-east-1:3183b2b8-e007-4828-95da-e42c4fd992e6",
   },
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_xwhwusxgzQ4qusctK3bGjSRP00gv3twIXE",
 }
